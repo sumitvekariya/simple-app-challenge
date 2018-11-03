753e97394414cba717de6c58b00c2e5381c82945
Sumit Vekariya <sumitvekariya7@gmail.com>
2018-11-03 10:29:49 +0530

    App Skeleton created with route configurations

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { PostsComponent } from './posts/posts.component';
+
+const routes: Routes = [
+  {
+    path: 'admin',
+    component: UsersComponent
+  },
+  {
+    path: '',
+    component: PostsComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  declarations: [],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { PostsComponent } from './posts/posts.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    PostsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
